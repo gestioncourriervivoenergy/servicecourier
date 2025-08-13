5788861187f63bf76d1ad5b65ea981739e524412
gestioncourriervivoenergy <gestioncourriervivoenergy@gmail.com>
2025-08-13 16:32:54 +0000

    Update send_all_emails.py

--- send_all_emails.py
+++ send_all_emails.py
@@ -1,26 +1,14 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from send_email import send_email  # ta fonction d'envoi email, à adapter si besoin
+from send_email import send_email  # ta fonction d'envoi email
 
-# Charger le fichier .env.local (même dossier que ce script)
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST").strip()
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME").strip()
-DB_USER = os.getenv("DB_USER").strip()
-DB_PASSWORD = os.getenv("DB_PASSWORD").strip()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_connection():
-    print(f"Connexion avec DB_HOST={DB_HOST}, DB_USER={DB_USER}, DB_NAME={DB_NAME}")
-    return psycopg2.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD
-    )
+    return psycopg2.connect(DATABASE_URL)
 
 def send_all_emails_en_cours():
     try:
