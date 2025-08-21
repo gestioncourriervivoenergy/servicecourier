e0c446f6234e263b509700271b50df55ce13d689
Equipe Data Vivo Energy <gestioncourriervivoenergy@gmail.com>
2025-08-21 23:15:09 +0000

    Update clean.py

--- clean.py
+++ clean.py
@@ -24,6 +24,9 @@ def clean_email(email_str: str) -> str | None:
         "emmanuella_kouame": "emmanuella.kouame@vivoenergy.com",
         "paule_irene_diallo": "Paule-Irene.Diallo@vivoenergy.com",
         "eunice_achie": "eunice.achie@vivoenergy.com",
+        "eunice_achie_vivoenergy_com": "eunice.achie@vivoenergy.com",
+        "emma_yapi_vivoenergy_com":"Emma.Yapi@vivoenergy.com"
+        
     }
 
     corrections = {
