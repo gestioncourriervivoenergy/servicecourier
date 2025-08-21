75259d64aba6ceccb8b836f73b739c7efbb3b20a
Equipe Data Vivo Energy <gestioncourriervivoenergy@gmail.com>
2025-08-21 06:59:26 +0000

    Update send_all_emails.py

--- send_all_emails.py
+++ send_all_emails.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 from dotenv import load_dotenv
-from send_emailbrevo import send_email
+from send_email import send_email
 import logging
 from datetime import datetime
 
